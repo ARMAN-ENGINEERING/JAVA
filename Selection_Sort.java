5ca51c20681ff7c31eefd50f94d70585fe069457
ARMAN UMAR <97071435+arman-engineering@users.noreply.github.com>
2022-04-09 04:54:12 +0700

    Update Selection_Sort.java

--- Selection_Sort.java
+++ Selection_Sort.java
@@ -1,17 +1,16 @@
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class JavaApplication1 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         int panjang, i, j;
-        System.out.println("Panjang Array : ");
+        
         panjang = input.nextInt();
         ArrayList<Integer> arr = new ArrayList();
         
         for (i = 0; i <= panjang - 1; i++) {
-            System.out.println("Nilai index ke "+i+" : ");
             int nilai = input.nextInt();
             arr.add(nilai);
         }
@@ -19,20 +18,20 @@ public class JavaApplication {
             int jMin;
             
             jMin = i;
-            for (j = 1 + i; j <= panjang - 1; j++) {
+            for (j = i+1; j <= panjang - 1; j++) {
                 if (arr.get(j) < arr.get(jMin)) {
                     jMin = j;
                 }
             }
             if (jMin != i) {
-                int temp;
-                
+                int temp, a, b;
                 temp = arr.get(i);
-                arr.set(i, jMin);
+                arr.set(i, arr.get(jMin));
                 arr.set(jMin, temp);
             }
         }
         System.out.println(arr);
     }
 }
+
 
